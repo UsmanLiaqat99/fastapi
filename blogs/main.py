c7a83f15d8a1cce3d21435a2f99e1e210b17a0cf
Usman Liaqat <chusmanliaqat1020@gmail.com>
2023-02-10 15:25:39 +0500

    Initial COmmit

---
+++ blogs/main.py
@@ -0,0 +1,58 @@
+from fastapi import FastAPI, Depends, status,  HTTPException
+from . import schemas, models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
+app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+models.Base.metadata.create_all(engine)
+
+
+
+@app.post("/blog", status_code=status.HTTP_201_CREATED, tags=["Blogs"])
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get("/blogs", tags=["Blogs"])
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    if not blogs:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Data not available")
+    return blogs
+    
+@app.get("/blog/{id}", tags=["Blogs"])
+def getById(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Data with the id:{id} is not available")
+        
+    return blog
+
+@app.delete("/blog/{id}", tags=["Blogs"])
+def delete(id, db: Session = Depends(get_db)):
+    db.query(models.Blog).filter(models.Blog.id == id).delete(synchronize_session=False)
+    db.commit()
+    if not db.query(models.Blog).filter(models.Blog.id == id).first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Data with the id:{id} is not available")
+    return {"message": "Blog deleted"}
+
+@app.put("/blog/{id}", tags=["Blogs"])
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Data with the id:{id} is not available")
+    blog.update(request.dict())
+    db.commit()
+    return {"message": "Blog updated"}
--- main.py
+++ main.py
@@ -1,7 +1,50 @@
-from fastapi import FastAPI # This is a class
+from fastapi import FastAPI
+from typing import Optional
+from pydantic import BaseModel
 
-app = FastAPI() # This is an instance of the class
+app = FastAPI()
 
-@app.get("/") # This is a decorator
+
+@app.get("/")
 def index():
-    return {"message": "Hello World"} # This is a dictionary
+    return {"message": "Hello World"}
+
+
+@app.get("/about")
+def about():
+    return {"message": "About Page"}
+
+
+# -----    Blogs  ---
+@app.get("/blogs", tags=["blog"])
+def blogs(limit=10, published: bool = True):
+    if published:
+        return {"message": f"{limit} published blogs"}
+    else:
+        return {"message": f"{limit} blogs"}
+
+
+@app.get("/blog/{id}", tags=["blog"])
+def blog(id: int):
+    return {"message": id}
+
+
+@app.get("/blog/{id}/comments", tags=["blog"])
+def comments(id):
+    return {"message": {"1", "2"}}
+
+
+@app.get("/blog/{id}/comments/{comment_id}", tags=["blog"])
+def comment(id, comment_id):
+    return {"message": {"1", "2"}}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post("/blog", tags=["blog"])
+def create_blog(request: Blog):
+    return {"message": "Blog is Created"}
